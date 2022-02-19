8309c6bea87d0182e02cbb677e49a7df0e208e07
Mammadli999 <abbasfm@code.edu.az>
2022-02-19 20:01:02 +0400

    Ders

---
+++ ConsoleApp5.Task--Ders/Task 10/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Task_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] numberCharCodes = Enumerable.Range(48, 10).ToArray();
+            //string word = "1234abbas1234";
+
+            //string newText = Regex.Replace(word, @"=?\d+", string.Empty);
+
+            //for (int i = 0; i < word.Length; i++)
+            //{
+            //    byte charCode = (byte)word[i];
+
+            //    if(Array.IndexOf(numberCharCodes,charCode) == -1)
+
+            //    newText = newText + word[i];
+            //}
+
+            //Console.WriteLine(newText);
+           // DateTime date1 = DateTime.Now;
+           // DateTime date2 = DateTime.UtcNow;
+            DateTime date3 = DateTime.Today;
+           // Console.WriteLine(date1);
+           // Console.WriteLine(date2);
+            Console.WriteLine(date3);
+        }
+    }
+}
